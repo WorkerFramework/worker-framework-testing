1ef38532cc2dc8bcaacaa9850ee934718a43e30d
Raghavendran Gopalakrishnan <rgopalakris3@opentext.com>
2024-10-23 15:53:28 +0530

    US975260: Refactor HPE references
    https://internal.almoctane.com/ui/entity-navigation?p=131002/6001&entityType=work_item&id=975260

---
+++ workerframework-testing-integration/src/main/java/com/github/workerframework/worker/testing/preparation/ContentResultProcessor.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2022-2024 Open Text.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.workerframework.worker.testing.preparation;
+
+import com.github.cafapi.common.api.Codec;
+import com.github.cafapi.common.util.ref.ReferencedData;
+import com.github.workerframework.worker.api.DataStore;
+import com.github.workerframework.worker.api.DataStoreSource;
+import com.github.workerframework.worker.api.TaskMessage;
+import com.github.workerframework.worker.testing.FileTestInputData;
+import com.github.workerframework.worker.testing.ContentFileTestExpectation;
+import com.github.workerframework.worker.testing.TestConfiguration;
+import com.github.workerframework.worker.testing.TestItem;
+
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.util.function.Function;
+
+/**
+ * Created by ploch on 25/11/2015.
+ */
+public class ContentResultProcessor<TWorkerTask, TWorkerResult, TInput extends FileTestInputData, TExpected extends ContentFileTestExpectation> extends PreparationResultProcessor<TWorkerTask, TWorkerResult, TInput, TExpected>
+{
+    private final DataStore dataStore;
+    private final Function<TWorkerResult, ReferencedData> getContentFunc;
+
+    protected ContentResultProcessor(TestConfiguration<TWorkerTask, TWorkerResult, TInput, TExpected> configuration, Codec codec, DataStore dataStore, Function<TWorkerResult, ReferencedData> getContentFunc)
+    {
+        super(configuration, codec);
+        this.dataStore = dataStore;
+        this.getContentFunc = getContentFunc;
+    }
+
+    @Override
+    protected byte[] getOutputContent(TWorkerResult workerResult, TaskMessage message, TestItem<TInput, TExpected> testItem) throws Exception
+    {
+
+        Path contentFile = null;
+        ReferencedData textData = getContentFunc.apply(workerResult);
+        if (textData != null) {
+            InputStream dataStream = textData.acquire(new DataStoreSource(dataStore, getCodec()));
+
+            contentFile = saveContentFile(testItem, testItem.getTag(), "result", dataStream);
+        }
+
+        TExpected expectation = testItem.getExpectedOutputData();
+
+        expectation.setExpectedContentFile(contentFile == null ? null : contentFile.toString());
+        expectation.setExpectedSimilarityPercentage(80);
+
+        return super.getOutputContent(workerResult, message, testItem);
+    }
+}
